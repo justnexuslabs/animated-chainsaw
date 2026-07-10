bbb86d3109fd726e64fef618d2ef0072bf5dc09b
justnexuslabs <justnexuslabs@gmail.com>
2026-07-10 11:49:01 -0500

    Fix: Move navigation code to proper TypeScript configuration file

---
+++ src/config/navigation.ts
@@ -0,0 +1,19 @@
+export const navItems = [
+  { label: "Home", href: "/" },
+  { label: "About", href: "/about" },
+  { label: "Mission", href: "/mission" },
+  { label: "Genesis", href: "/genesis" }
+];
+
+export const footerResources = [
+  { label: "Docs", href: "/docs" },
+  { label: "Whitepaper", href: "/whitepaper" },
+  { label: "FAQ", href: "/faq" },
+  { label: "Blog", href: "/blog" }
+];
+
+export const connectLinks = [
+  { label: "Discord", href: "https://discord.gg/donlien" },
+  { label: "Twitter", href: "https://twitter.com/donlien" },
+  { label: "GitHub", href: "https://github.com/justnexuslabs" }
+];
